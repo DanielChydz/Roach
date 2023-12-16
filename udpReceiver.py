94563c9ad5dfe68be821d8a092a5d0a8518ce29c
DanielChydz <daniel.chydz@gmail.com>
2023-12-16 02:42:35 +0100

    Further work

--- udpReceiver.py
+++ udpReceiver.py
@@ -19,22 +19,20 @@ with open(csv_filename, 'w', newline='') as csvfile:
             data, addr = sock.recvfrom(1024)
 
             message = data.decode('utf-8')
+            if message.endswith('E'):
+                print("Program zakończył działanie.")
+                sys.exit(0)
+
             if message.startswith('DC_Remote_Car_Key'):
                 index_l = message.find('L')
                 index_p = message.find('P')
 
                 if index_l != -1 and index_p != -1:
                     value_l = message[index_l + 1:index_p]
-                    if message.endswith('E'):
-                        value_p = message[index_p + 1:message.find('E')]
-                    else:
-                        value_p = message[index_p + 1:]
+                    value_p = message[index_p + 1:]
 
                     csv_writer.writerow([value_l, value_p])
-
-            if message.endswith('E'):
-                print("Program zakończył działanie.")
-                sys.exit(0)
+            
     except Exception as e:
         print(f"Wystąpił błąd: {e}")
     finally:
